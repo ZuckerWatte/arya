eedec43229847bfb15adaff4e193f5478bb59c77
Mona Fenzl <mona.fenzl@web.de>
2019-08-22 00:42:30 +0200

    add material icons and styling

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Map from './components/Map.vue';
+import checkBoldIcon from "vue-material-design-icons/CheckBold.vue"
+import closeCircleIcon from "vue-material-design-icons/CloseCircle.vue"
+ 
+// global icon components 
+Vue.component("check-bold-icon", checkBoldIcon)
+Vue.component("close-circle-icon", closeCircleIcon)
 
 @Component({
   components: {
@@ -14,15 +20,27 @@ import Map from './components/Map.vue';
   },
 })
 export default class App extends Vue {}
+
 </script>
 
 <style>
+html, body {
+  max-height: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+} 
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+.check .material-design-icon > .material-design-icon__svg {
+  bottom: 0.125rem !important;
 }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="hello">
-    <p v-if="loading">Loading...</p>
-    <h1>{{header}}</h1>
-    <p>{{country.name}}</p>
-    <img :src="country.flag" :alt="'Flag of ' + country.name"/>
-    <button @click="nextRandomCountry" class="btn btn-primary">next!</button>
+  <div class="map">
+    <div class="country-content">
+      <p v-if="loading">Loading...</p>
+      <h1>{{header}}</h1>
+      <p>{{country.name}}</p>
+      <img :src="country.flag" :alt="'Flag of ' + country.name" />
+    </div>
+    <div class="navigation">
+      <button @click="nextRandomCountry" class="round-button cancel">
+        <close-circle-icon />
+      </button>
+      <button class="round-button check">
+        <check-bold-icon />
+      </button>
+    </div>
   </div>
 </template>
 
@@ -68,7 +77,74 @@ button {
 }
 
 img {
-    width: 50%;
+  height: auto;
+  height: 75%;
+}
+
+.map {
+  height: 100%;
+}
+
+.country-content {
+  display: block;
+  height: 75%;
+  overflow: hidden;
+}
+
+.navigation {
+  display: block;
+  height: 25%;
+}
+
+.round-button {
+  border: none;
+  display: inline-block;
+  margin: 2rem;
+  padding: 0;
+  height: auto;
+}
+
+.round-button:hover span{
+  cursor: pointer;
+  transform: scale(1.5);
+  transition-duration: 0.35s;
+  transition-property: color, border, transform;
+}
+
+.round-button span {
+  width: 3rem;
+  height: 3rem;
+  border: 2px solid;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  transform: scale(1.4);
+  transition-duration: 0.2s;
+  transition-property: color, border, transform;
+}
+
+.cancel span {
+  font-size: 4rem;
+  border-color: #f44336;
+  color: #f44336;
+}
+
+.cancel:hover span{ 
+  border-color: #f44336cb;
+  color: #f44336cb;
+}
+
+.check span {
+  font-size: 2.5rem;
+  border-color: #4caf50;
+  color: #fff;
+  background-color: #4caf50;
+}
+
+.check:hover span {
+  border-color: #4caf4fd6;
+  color: #fff;
+  background-color: #4caf4fd6;
 }
 
 </style>
